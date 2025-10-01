122501817573f6a34a40bab856350b05418bbbbd
inovacomfernando <fernandoluizsouzaramalho@gmail.com>
2025-10-01 15:10:04 -0300

    Update app.py

--- dashboard-marketing-saas/app.py
+++ dashboard-marketing-saas/app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import gspread
-from google.oauth2.service_account import Credentials
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.subplots import make_subplots
